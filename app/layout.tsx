7e0811de194d7377d836a7a64262172db77f37ee
emmanuelxs <ilovemakinwids@gmail.com>
2026-05-04 14:51:48 -0700

    add force-dynamic to fix build

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
-
+export const dynamic = 'force-dynamic';
 export const metadata: Metadata = {
   title: "MrTheManWEED",
   description: "Your personal Stock Market Signals",
